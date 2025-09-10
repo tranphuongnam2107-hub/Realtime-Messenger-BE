74965faa460dfd2195e9ef021de18c4eab66cb9f
tranphuongnamstudy <tranphuongnam.2107@gmail.com>
2025-09-10 20:02:58 +0700

    Add: database connection, model, and base singleton DAO

--- Models/Model/Friend.cs
+++ Models/Model/Friend.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Models.Model
 {
     public class Friend
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string FriendId { get; set; }
+        [Required]
+        public string SenderId { get; set; }
+        [Required]
+        public string ReceiverId { get; set; }
+        [Required]
+        public string StatusFriend { get; set; }
+
     }
 }
